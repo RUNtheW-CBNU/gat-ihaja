4927a87037c5a41686411d7bf7fd0749d9e46946
JINY0609 <ok373737@naver.com>
2022-04-05 13:15:27 +0900

    Initial commit

---
+++ src/main/java/com/example/web/domain/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.example.web.domain.repository;
+
+import com.example.web.domain.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByEmail(String userEmail);
+}
